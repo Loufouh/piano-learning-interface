4ac747115aee86bbd6e10182c55e0b8730aaf4b1
Louis700 <louis.masson.contact@gmail.com>
2026-01-13 17:35:21 +0100

    API - Add fixtures (user + spaceItems)

--- API/prisma/seed.ts
+++ API/prisma/seed.ts
@@ -12,6 +12,64 @@ async function main() {
 			isManager: true,
 		},
 	});
+	await prisma.user.create({
+		data: {
+			name: "TestUser",
+			email: "user@mail.com",
+			password: await bcrypt.hash(process.env.TEST_MANAGER_PASSWORD, 10),
+			isManager: false,
+		},
+	});
+	const manager = (await prisma.user.findUnique({
+		where: { email: "manager@mail.com" },
+	})) || { id: -1 };
+	const user = (await prisma.user.findUnique({
+		where: { email: "user@mail.com" },
+	})) || { id: -1 };
+
+	await prisma.spaceItem.createMany({
+		data: [
+			{
+				text: "Bonjour",
+			},
+			{
+				text: "Au revoir",
+			},
+			{
+				text: "Hé, je suis là aussi :3",
+			},
+			{
+				text: "Tu vas bien ? :eyes:",
+			},
+		],
+	});
+	const spaceItems = await prisma.spaceItem.findMany();
+	await prisma.spaceItemsOnUsers.createMany({
+		data: [
+			// Manager
+			{
+				order: 0,
+				spaceItemId: spaceItems[0].id,
+				userId: manager.id,
+			},
+			{
+				order: 1,
+				spaceItemId: spaceItems[2].id,
+				userId: manager.id,
+			},
+			{
+				order: 2,
+				spaceItemId: spaceItems[1].id,
+				userId: manager.id,
+			},
+			// User
+			{
+				order: 0,
+				spaceItemId: spaceItems[3].id,
+				userId: user.id,
+			},
+		],
+	});
 
 	console.log("✅ Fixtures insérées avec succès !");
 }
